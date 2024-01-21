b8d8f2434ec677181be76a8c069fc2e17c1ee257
Khang Ho <nguyenkhang.ziis.nd@gmail.com>
2024-01-21 21:26:49 +0700

    add production check for CORS

--- src/app.rs
+++ src/app.rs
@@ -1,17 +1,24 @@
-use axum::Router;
+use axum::{http::HeaderValue, Router};
 use tower_http::{
-    cors::{Any, CorsLayer},
+    cors::CorsLayer,
     services::{ServeDir, ServeFile},
 };
 
 use crate::routes;
 
 pub async fn create_app() -> Router {
+    let is_production = std::env::var("PRODUCTION");
+
+    let origin = match is_production {
+        Ok(..) => "https://productionurl.com".parse::<HeaderValue>().unwrap(),
+        Err(..) => "*".parse::<HeaderValue>().unwrap(),
+    };
+
     Router::new()
         .nest_service(
             "/",
             ServeDir::new("static").fallback(ServeFile::new("/404.html")),
         )
         .merge(routes::user::create_route())
-        .layer(CorsLayer::new().allow_origin(Any))
+        .layer(CorsLayer::new().allow_origin(origin))
 }
